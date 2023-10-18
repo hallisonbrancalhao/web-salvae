873a06ea6b5a49adf7a08eb2625f7f626c61fb86
Tiago Ramos Louzada <ra-20154520-2@alunos.unicesumar.edu.br>
2023-10-17 21:26:42 -0300

    feat: 🎸 ajuste layout dashbord

---
+++ src/framework/constantes/URL.API.tsx
@@ -0,0 +1,3 @@
+export const URL_BASE = 'https://crudcrud.com/api/1ca48961a1af4c8eaedc066694a01d55';
+export const URL_RESTAURANTE = `${URL_BASE}/restaurante`
+export const URL_CUPOM = `${URL_BASE}/cupom`
